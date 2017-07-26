e9ade2daf8dfed5907f38f0ad52fe87890d67e63
SamareshB <sam@pocketapp.in>
2017-07-26 14:02:05 +0530

    Image file Added

--- test.js
+++ test.js
@@ -5,5 +5,5 @@ var fileYo=require('fs');
 var https=require('https');
 fileYo.writeFile(__dirname+"/index.html", "<h1> Welcome to the World of Node </h1>");
 var fboLogo="https://freshboxoffice.com/img/fbo-w.png";
-var fboFile=fileYo.create.WriteStream(__dirname + "/fbologo.jpg");
-var request=https.get(fboLogo,function(response){response.pipe()});
+var fboFile=fileYo.createWriteStream(__dirname + "/fbologo.jpg");
+var request=https.get(fboLogo,function(response){response.pipe();});
